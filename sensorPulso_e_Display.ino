fef8a8d46190c1027c581bb221c47150fdc13c02
DekyDeky <166323985+DekyDeky@users.noreply.github.com>
2024-10-03 12:19:07 -0300

    Alteração nos comentários

--- sensorPulso_e_Display.ino
+++ sensorPulso_e_Display.ino
@@ -1,8 +1,8 @@
-#include <Adafruit_GFX.h>      // Biblioteca gráfica
-#include <Adafruit_ST7735.h>   // Biblioteca do driver ST7735
+#include <Adafruit_GFX.h>      
+#include <Adafruit_ST7735.h>   
 #include <PulseSensorPlayground.h>
 
-// Defina os pinos de controle
+// pinos de controle
 #define TFT_CS     10   // Chip Select
 #define TFT_RST    9    // Reset (ou -1 se não for usado)
 #define TFT_DC     8    // Data/Command
@@ -18,7 +18,7 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST); //Cria instânci
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(115200); 
     pulseSensor.analogInput(PulseWire);   
     pulseSensor.blinkOnPulse(LED);       
     pulseSensor.setThreshold(Threshold);
@@ -29,43 +29,45 @@ void setup() {
   
 
   // Inicializa a comunicação com a tela
-  tft.initR(INITR_GREENTAB);  // Configuração padrão para ST7735S (com a aba preta)
+  tft.initR(INITR_GREENTAB); 
   tft.invertDisplay(true);
   
   // Prepara a tela
   tft.fillScreen(ST7735_BLACK);  // Preenche a tela com cor preta
   tft.setTextColor(ST7735_WHITE); // Define a cor do texto como branca
-  tft.setTextSize(2);  // Define o tamanho do texto (1 é o menor)
+  tft.setTextSize(2); 
   
-  // Exibe "Hello, World!" no meio da tela
   tft.setCursor(28, 20);  // Define a posição inicial do texto (x, y)
-  tft.println("IFTECH");
+  tft.println("IFTECH"); // Define o texto
   
   tft.setTextSize(1);
-  tft.setCursor(38, 40);  // Define a nova posição
+  tft.setCursor(38, 40);  
   tft.println("Sensor de");
   tft.setCursor(50, 50);
   tft.println("Pulso");
 
   tft.fillCircle(55, 80, 10, ST7735_RED);   // Primeira metade do coração (circulo esquerdo)
   tft.fillCircle(75, 80, 10, ST7735_RED);   // Segunda metade do coração (circulo direito)
-  tft.fillTriangle(65, 105, 45, 80, 85, 80, ST7735_RED);
+  tft.fillTriangle(65, 105, 45, 80, 85, 80, ST7735_RED); //Ponta do Coração
 }
 
 void loop() {
-  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
-    int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
-                                                  // "myBPM" hold this BPM value now. 
+  if (pulseSensor.sawStartOfBeat()) {            // Checa se o sensor captou algum sinal
+    int myBPM = pulseSensor.getBeatsPerMinute();  // Chama a função que retorna o BPM e guarda na variável.
+
+    //Testes do Sensor, não sendo necessário no produto final.
     Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
     Serial.print("BPM: ");                        // Print phrase "BPM: " 
     Serial.println(myBPM);                        // Print the value inside of myBPM.
 
+    //Reseta o bpm
     tft.fillRect(0, 120, 100, 30, ST7735_BLACK);
 
+    //Imprime o bpm
     tft.setTextSize(2);
     tft.setCursor(50, 120);
     tft.println(myBPM);
   }
 
-    delay(1000);                    // considered best practice in a simple sketch.
+    delay(1000);                    // delay de 1 segundo para o usuário conseguir entender.
 }
